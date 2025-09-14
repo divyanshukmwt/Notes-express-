1d1499504dbc552606a4c3beaa1c45c0e71ba012
divyanshukmwt0408 <officialdivyanshu18@gmail.com>
2025-09-14 13:56:03 +0530

    notes CRUD Complete

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
-const express = require('express');
+const express = require("express");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.post("/notes",(req,res)=>{
-    console.log(req.body)
+let notes = [];
+
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  res.json({
+    message: "note created sucessfully",
+  });
+});
+
+app.delete("/notes/:index",(req,res)=>{
+    const index = req.params.index
+    delete notes[index]
+    res.json({
+        message:"note deleted successfully",
+    })
 })
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
+app.patch("/notes/:index",(req,res)=>{
+    const index = req.params.index
+    const { title, desc } = req.body;
+
+    notes[index].title = title;
+    notes[index].desc = desc;
+
+    res.json({
+        message:"note updated succesfully",
+    })
 })
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
